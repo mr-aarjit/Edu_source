1301db75ec8b8ee130f770832c74260bb6878881
mr-aarjit <aarjitbaral2067@gmail.com>
2026-07-04 10:34:35 +0545

    Fix production settings for Render

--- Project/settings.py
+++ Project/settings.py
@@ -24,10 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-)6gpoln&166twi2fio7%g)gb10v*ye^d7c7fmggtep$uo2&*oh'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
-
+DEBUG = False
+SECRET_KEY = os.environ.get("SECRET_KEY")
+ALLOWED_HOSTS = [".onrender.com"]
 
 # Application definition
 
@@ -118,7 +117,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR/ "staticfiles"
 
 STATICFILES_DIRS = [
